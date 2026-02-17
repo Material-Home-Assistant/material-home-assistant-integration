c92dadaeae2c18e318851155c834339df09fd920
Giovanni <giovannilamarmora.working@gmail.com>
2026-02-17 17:00:04 +0100

    version: 🧩 Updated version

--- custom_components/material_home_assistant/const.py
+++ custom_components/material_home_assistant/const.py
@@ -4,7 +4,7 @@ DOMAIN = "material_home_assistant"
 
 # -------------------------------------------------------------
 # Costanti di configurazione dell'integrazione (Entità e Dispositivo)
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 DEVICE_NAME = "Material Home Assistant"
 DEVICE_MODEL = "License Manager"
 DOCUMENTATION_URL = "https://material-home-assistant.giovannilamarmora.com"
